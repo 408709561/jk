d14d0c2add26fdff382b02dc35ba6f3e35cbdef0
王浩彬 <mail2ace@163.com>
2018-01-31 23:33:55 +0800

    [Feature] 增加字典值获取

--- ace-dict/src/main/java/com/github/wxiaoqi/security/dict/rest/DictValueController.java
+++ ace-dict/src/main/java/com/github/wxiaoqi/security/dict/rest/DictValueController.java
@@ -1,13 +1,25 @@
 package com.github.wxiaoqi.security.dict.rest;
 
+import com.github.wxiaoqi.security.common.msg.TableResultResponse;
 import com.github.wxiaoqi.security.common.rest.BaseController;
 import com.github.wxiaoqi.security.dict.biz.DictValueBiz;
 import com.github.wxiaoqi.security.dict.entity.DictValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("dictValue")
 public class DictValueController extends BaseController<DictValueBiz,DictValue> {
-
+    @RequestMapping(value = "/type/{code}",method = RequestMethod.GET)
+    public TableResultResponse<DictValue> getDictValueByDictTypeCode(@PathVariable("code") String code){
+        Example example = new Example(DictValue.class);
+        example.createCriteria().andLike("code",code+"%");
+        List<DictValue> dictValues = this.baseBiz.selectByExample(example);
+        return new TableResultResponse<DictValue>(dictValues.size(),dictValues);
+    }
 }
