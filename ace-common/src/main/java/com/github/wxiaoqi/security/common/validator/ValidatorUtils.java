462c2b557f59dbda599936691c8c220cd08040e2
王浩彬 <mail2ace@163.com>
2018-03-05 12:44:10 +0800

    [Feature] 增加附件处理模块

---
+++ ace-common/src/main/java/com/github/wxiaoqi/security/common/validator/ValidatorUtils.java
@@ -0,0 +1,70 @@
+/*
+ *
+ *  *  Copyright (C) 2018  Wanghaobin<463540703@qq.com>
+ *
+ *  *  AG-Enterprise 企业版源码
+ *  *  郑重声明:
+ *  *  如果你从其他途径获取到，请告知老A传播人，奖励1000。
+ *  *  老A将追究授予人和传播人的法律责任!
+ *
+ *  *  This program is free software; you can redistribute it and/or modify
+ *  *  it under the terms of the GNU General Public License as published by
+ *  *  the Free Software Foundation; either version 2 of the License, or
+ *  *  (at your option) any later version.
+ *
+ *  *  This program is distributed in the hope that it will be useful,
+ *  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *  GNU General Public License for more details.
+ *
+ *  *  You should have received a copy of the GNU General Public License along
+ *  *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+package com.github.wxiaoqi.security.common.validator;
+
+
+import com.github.wxiaoqi.security.common.exception.base.BusinessException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+/**
+ * hibernate-validator校验工具类
+ * <p>
+ * 参考文档：http://docs.jboss.org/hibernate/validator/5.4/reference/en-US/html_single/
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-15 10:50
+ */
+public class ValidatorUtils {
+    private static Validator validator;
+
+    static {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    /**
+     * 校验对象
+     *
+     * @param object 待校验对象
+     * @param groups 待校验的组
+     * @throws BusinessException 校验不通过，则报RRException异常
+     */
+    public static void validateEntity(Object object, Class<?>... groups)
+            throws BusinessException {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
+        if (!constraintViolations.isEmpty()) {
+            StringBuilder msg = new StringBuilder();
+            for (ConstraintViolation<Object> constraint : constraintViolations) {
+                msg.append(constraint.getMessage()).append("<br>");
+            }
+            throw new BusinessException(msg.toString());
+        }
+    }
+}
