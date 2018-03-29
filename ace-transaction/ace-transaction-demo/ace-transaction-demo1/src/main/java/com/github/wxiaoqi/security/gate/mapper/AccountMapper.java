ddb845422ae6bfa9a02c8980776c9dd0adeb0c97
王浩彬 <mail2ace@163.com>
2018-03-29 15:35:48 +0800

    [Feature] 优化异常

--- ace-transaction/ace-transaction-demo/ace-transaction-demo1/src/main/java/com/github/wxiaoqi/security/gate/mapper/AccountMapper.java
+++ ace-transaction/ace-transaction-demo/ace-transaction-demo1/src/main/java/com/github/wxiaoqi/security/gate/mapper/AccountMapper.java
@@ -23,8 +23,8 @@
 
 package com.github.wxiaoqi.security.gate.mapper;
 
+import com.github.wxiaoqi.security.common.mapper.CommonMapper;
 import com.github.wxiaoqi.security.gate.entity.Account;
-import tk.mybatis.mapper.common.Mapper;
 
 /**
  * 
@@ -33,6 +33,6 @@ import tk.mybatis.mapper.common.Mapper;
  * @email 463540703@qq.com
  * @version 2018-01-18 11:02:17
  */
-public interface AccountMapper extends Mapper<Account> {
+public interface AccountMapper extends CommonMapper<Account> {
 	
 }
--- ace-transaction/ace-transaction-demo/ace-transaction-demo2/src/main/java/com/github/wxiaoqi/security/gate/mapper/ProdMapper.java
+++ ace-transaction/ace-transaction-demo/ace-transaction-demo2/src/main/java/com/github/wxiaoqi/security/gate/mapper/ProdMapper.java
@@ -23,8 +23,8 @@
 
 package com.github.wxiaoqi.security.gate.mapper;
 
+import com.github.wxiaoqi.security.common.mapper.CommonMapper;
 import com.github.wxiaoqi.security.gate.entity.Prod;
-import tk.mybatis.mapper.common.Mapper;
 
 /**
  * 
@@ -33,6 +33,6 @@ import tk.mybatis.mapper.common.Mapper;
  * @email 463540703@qq.com
  * @version 2018-01-18 11:11:06
  */
-public interface ProdMapper extends Mapper<Prod> {
+public interface ProdMapper extends CommonMapper<Prod> {
 	
 }
