d65e0825d0265766e6ae032baa2825c81850c2ad
minull <3332132702@qq.com>
2018-05-16 14:18:41 +0800

    [Feature] 还原lib

---
+++ lib/mvvn.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dmaven.test.skip=true -Dfile=jwt-core-4.0-SNAPSHOT.jar -DgroupId=com.github.wxiaoqi -DartifactId=jwt-core -Dversion=4.0-SNAPSHOT -Dpackaging=jar
+
+mvn install:install-file -Dmaven.test.skip=true -Dfile=ace-merge-1.0-SNAPSHOT.jar -DgroupId=com.github.wxiaoqi -DartifactId=ace-merge -Dversion=1.0-SNAPSHOT -Dpackaging=jar
