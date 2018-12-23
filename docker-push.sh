0c02def353a0f9567b98d5ee985757641facec3a
408709561 <408709561@qq.com>
2018-12-23 17:02:04 +0800

    ..
    ...

--- docker-push.sh
+++ docker-push.sh
@@ -14,40 +14,40 @@ push_tag()
 {
     set -e
     # ace-config
-    docker tag ag/ace-config:latest $REGISTRY_URL/$NAME_SPACE/ace-config:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-config:$BUILD_TAG
+    docker tag ag/ace-config:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-config:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-config:test1001
     docker rmi ag/ace-config:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-config:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-config:test1001
 
     # ace-auth
-    docker tag ag/ace-auth:latest $REGISTRY_URL/$NAME_SPACE/ace-auth:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-auth:$BUILD_TAG
+    docker tag ag/ace-auth:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-auth:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-auth:test1001
     docker rmi ag/ace-auth:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-auth:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-auth:test1001
 
     # ace-gate
-    docker tag ag/ace-gate:latest $REGISTRY_URL/$NAME_SPACE/ace-gate:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-gate:$BUILD_TAG
+    docker tag ag/ace-gate:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-gate:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-gate:test1001
     docker rmi ag/ace-gate:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-gate:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-gate:test1001
 
     # ace-admin
-    docker tag ag/ace-admin:latest $REGISTRY_URL/$NAME_SPACE/ace-admin:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-admin:$BUILD_TAG
+    docker tag ag/ace-admin:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-admin:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-admin:test1001
     docker rmi ag/ace-admin:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-admin:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-admin:test1001
 
     # ace-center
-    docker tag ag/ace-center:latest $REGISTRY_URL/$NAME_SPACE/ace-center:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-center:$BUILD_TAG
+    docker tag ag/ace-center:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-center:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-center:test1001
     docker rmi ag/ace-center:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-center:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-center:test1001
 
     # ace-dict
-    docker tag ag/ace-dict:latest $REGISTRY_URL/$NAME_SPACE/ace-dict:$BUILD_TAG
-    docker push $REGISTRY_URL/$NAME_SPACE/ace-dict:$BUILD_TAG
+    docker tag ag/ace-dict:latest registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-dict:test1001
+    docker push registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-dict:test1001
     docker rmi ag/ace-dict:latest
-    docker rmi $REGISTRY_URL/$NAME_SPACE/ace-dict:$BUILD_TAG
+    docker rmi registry.cn-shenzhen.aliyuncs.com/jk_basic/ace-dict:test1001
 }
 
 if [ "$1" = "test" ];
